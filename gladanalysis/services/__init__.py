14df927889bc1b09f286f91992b394e3bddb9931
astrong19 <astrong@wri.org>
2017-05-19 17:13:18 -0400

    init service class

--- gladanalysis/services/__init__.py
+++ gladanalysis/services/__init__.py
@@ -1,1 +1,7 @@
+"""gfwanalysis SERVICES MODULE"""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from gladanalysis.services.geostore_service import GeostoreService
