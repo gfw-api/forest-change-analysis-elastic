1737f7da88b740d7cc05ce0c6bd476497de9415d
astrong19 <astrong@wri.org>
2017-05-19 18:01:12 -0400

    typo

--- gladanalysis/services/__init__.py
+++ gladanalysis/services/__init__.py
@@ -7,4 +7,4 @@ from __future__ import print_function
 from gladanalysis.services.geostore_service import GeostoreService
 from gladanalysis.services.date_service import DateService
 from gladanalysis.services.sql_service import SqlService
-from gladanalysis.services.sql_service import AnalysisService
+from gladanalysis.services.analysis_service import AnalysisService
