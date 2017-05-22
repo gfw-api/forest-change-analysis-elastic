8dbd546a507fa64b65a60d3eaf3f83944062b50e
astrong19 <astrong@wri.org>
2017-05-22 15:37:31 -0400

    add helper comments

--- gladanalysis/routes/api/v1/ms_router.py
+++ gladanalysis/routes/api/v1/ms_router.py
@@ -30,14 +30,13 @@ def query_glad():
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
-
+    #get sql and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date)
 
-
-    #query glad database
+    #send sql and geostore to analysis service to query elastic database
     data = AnalysisService.make_glad_request(sql, geostore)
 
     #make request to geostore to get area in hectares
@@ -57,21 +56,22 @@ def glad_country(iso_code):
 
     logging.info('Running country level glad analysis')
 
-    #accept period parameter
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date, iso_code)
 
-    #get geostore id from admin areas and total area of geostore request
+    #get area in hectares from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code)
 
-    #make request to glad database
+    #make analysis request to the GLAD elastic database
     data = AnalysisService.make_glad_request(sql)
 
+    #standardize response from analysis service
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Glad', data, "COUNT(julian_day)", datasetID, download_sql, area_ha)
 
@@ -83,23 +83,24 @@ def glad_country(iso_code):
 
 def glad_admin(iso_code, admin_id):
 
-    logging.info('Running GADM level glad analysis')
+    logging.info('Running state level glad analysis')
 
-    #accept period parameter
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date, iso_code, admin_id)
 
-    #get geostore id from admin areas and total area of geostore request
+    #get area in hectares from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code, admin_id)
 
-    #query glad database
+    #send query to GLAD elastic database through analysis service
     data = AnalysisService.make_glad_request(sql)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Glad', data, "COUNT(julian_day)", datasetID, download_sql, area_ha)
 
@@ -111,22 +112,24 @@ def glad_admin(iso_code, admin_id):
 
 def glad_dist(iso_code, admin_id, dist_id):
 
-    logging.info('Running GADM level glad analysis')
+    logging.info('Running district level glad analysis')
 
-    #accept period parameter
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date, iso_code, admin_id, dist_id)
 
+    #get area in hectares from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code, admin_id)
 
-    #query glad database
+    #send query to glad elastic databse through analysis service
     data = AnalysisService.make_glad_request(sql)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Glad', data, "COUNT(julian_day)", datasetID, download_sql, area_ha)
 
@@ -138,21 +141,24 @@ def glad_dist(iso_code, admin_id, dist_id):
 
 def glad_use(use_type, use_id):
 
-    logging.info('QUERY GLAD BY LAND USE DATA')
+    logging.info('Intersecting GLAD with Land Use data')
 
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date)
 
+    #get geostore ID and area in hectares from geostore
     geostore, area = GeostoreService.make_use_request(use_type, use_id)
 
-    #make request to glad database
+    #send query to Glad elastic database through analysis service
     data = AnalysisService.make_glad_request(sql, geostore)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Glad', data, "COUNT(julian_day)", datasetID, download_sql, area, geostore)
 
@@ -168,17 +174,20 @@ def glad_wdpa(wdpa_id):
 
     period = request.args.get('period', None)
     conf = request.args.get('gladConfirmOnly', None)
-    
+
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_glad_sql(conf, from_year, from_date, to_year, to_date)
 
+    #Get geostore ID and area in hectares from geostore
     geostore, area = GeostoreService.make_wdpa_request(wdpa_id)
 
-    #make request to glad database
+    #send query to Glad elastic database through analysis service
     data = AnalysisService.make_glad_request(sql, geostore)
 
+    #Standardize response
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Glad', data, "COUNT(julian_day)", datasetID, download_sql, area, geostore)
 
@@ -187,6 +196,8 @@ def glad_wdpa(wdpa_id):
 @endpoints.route('/gladanalysis/date-range', methods=['GET'])
 def glad_date_range():
 
+    logging.info('Creating Glad Date Range')
+
     max_sql = '?sql=select MAX(julian_day)from {} where year = 2017'.format(os.getenv('GLAD_INDEX_ID'))
     min_sql = '?sql=select MIN(julian_day)from {} where year = 2015'.format(os.getenv('GLAD_INDEX_ID'))
 
@@ -194,16 +205,20 @@ def glad_date_range():
     datasetID = '{}'.format(os.getenv('GLAD_DATASET_ID'))
     max_julian = DateService.get_date(datasetID, max_sql, 'MAX(julian_day)')
 
+    #Set min and max julian values (min value shouldn't change, hence hard-code)
     max_value = max_julian + 1700
     # min_value = min_julian + 1500
     min_value = 1501
 
+    #format day
     max_day = datetime.datetime.strptime(str(max_value), '%y%j').date()
     min_day = datetime.datetime.strptime(str(min_value), '%y%j').date()
 
+    #format date
     max_date = max_day.strftime('%Y-%m-%d')
     min_date = min_day.strftime('%Y-%m-%d')
 
+    #standardize date response
     response = ResponseService.format_date_range("Glad", min_date, max_date)
 
     return jsonify({'data': response}), 200
@@ -215,22 +230,24 @@ def glad_date_range():
 
 def query_terrai():
 
-    logging.info('QUERYING TERRA I')
+    logging.info('Query Terra I by Geostore')
 
     geostore = request.args.get('geostore', None)
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #create conditions that issue correct sql
+    #grab query and download sql from sql service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date)
 
-    #format request parameters to Terra I
+    #send query to terra i elastic database
     data = AnalysisService.make_terrai_request(sql, geostore)
 
-    #get area from geostore
+    #get area of request in hectares from geostore
     area = GeostoreService.make_area_request(geostore)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area, geostore)
 
@@ -242,21 +259,23 @@ def query_terrai():
 
 def terrai_country(iso_code):
 
-    logging.info('QUERYING TERRA I AT COUNTRY LEVEL')
+    logging.info('Running Terra I country analysis')
 
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send dates to sql formatter
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date, iso_code)
 
-    #get geostore id from admin areas and total area of geostore request
+    #get area in hectares of response from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code)
 
-    #make request to terra i dataset
+    #send query to terra i elastic database
     data = AnalysisService.make_terrai_request(sql)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area_ha)
 
@@ -267,21 +286,23 @@ def terrai_country(iso_code):
 @validate_admin
 
 def terrai_admin(iso_code, admin_id):
-    logging.info('QUERYING TERRA I AT GADM LEVEL')
+    logging.info('Running Terra I state analysis')
 
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send dates to sql formatter
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date, iso_code, admin_id)
 
-    #get geostore id from admin areas and total area of geostore request
+    #get area in hectares of request from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code, admin_id)
 
-    #Make request to terra i dataset
+    #send query to terra i elastic database through analysis service
     data = AnalysisService.make_terrai_request(sql)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area_ha)
 
@@ -292,21 +313,23 @@ def terrai_admin(iso_code, admin_id):
 @validate_admin
 
 def terrai_dist(iso_code, admin_id, dist_id):
-    logging.info('QUERYING TERRA I AT GADM LEVEL')
+    logging.info('Running Terra I Analysis on District')
 
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send dates to sql formatter
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date, iso_code, admin_id, dist_id)
 
-    #get area of request
+    #get area in hectares of request from geostore
     area_ha = GeostoreService.make_gadm_request(iso_code, admin_id, dist_id)
 
-    #Make request to terra i dataset
+    #send query to terra i elastic database through analysis service
     data = AnalysisService.make_terrai_request(sql)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area_ha)
 
@@ -318,20 +341,23 @@ def terrai_dist(iso_code, admin_id, dist_id):
 
 def terrai_use(use_type, use_id):
 
-    logging.info('QUERY GLAD BY LAND USE DATA')
+    logging.info('Intersect Terra I and Land Use data')
 
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date)
 
+    #get geostore ID and area in hectares of request from geostore
     geostore, area = GeostoreService.make_use_request(use_type, use_id)
 
-    #make request to glad database
+    #send query to terra i elastic database through analysis service
     data = AnalysisService.make_terrai_request(sql, geostore)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area, geostore)
 
@@ -343,20 +369,23 @@ def terrai_use(use_type, use_id):
 
 def terrai_wdpa(wdpa_id):
 
-    logging.info('QUERY TERRA I BY WDPA')
+    logging.info('Intersect Terra I and WDPA')
 
     period = request.args.get('period', None)
 
+    #format period request to julian dates
     from_year, from_date, to_year, to_date = DateService.date_to_julian_day(period)
 
-    #send to sql formatter function
+    #get query and download sql from sql format service
     sql, download_sql = SqlService.format_terrai_sql(from_year, from_date, to_year, to_date)
 
+    #get geostore id and area in hectares of request from geostore
     geostore, area = GeostoreService.make_wdpa_request(wdpa_id)
 
-    #make request to glad database
+    #Send query to Terra I elastic database through analysis service
     data = AnalysisService.make_terrai_request(sql, geostore)
 
+    #standardize response
     datasetID = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
     standard_format = ResponseService.standardize_response('Terrai', data, "COUNT(day)", datasetID, download_sql, area, geostore)
 
@@ -364,6 +393,8 @@ def terrai_wdpa(wdpa_id):
 
 @endpoints.route('/terraianalysis/date-range', methods=['GET'])
 def terrai_date_range():
+
+    logging.info('Creating Terra I Date Range')
 
     max_sql = '?sql=select MAX(day)from {} where year = 2017'.format(os.getenv('TERRAI_INDEX_ID'))
     min_sql = '?sql=select MIN(day)from {} where year = 2004'.format(os.getenv('TERRAI_INDEX_ID'))
@@ -382,6 +413,7 @@ def terrai_date_range():
     max_date = max_day.strftime('%Y-%m-%d')
     min_date = '2004-01-01'
 
+    #standardize response
     response = ResponseService.format_date_range("Terrai", min_date, max_date)
 
     return jsonify({'data': response}), 200
