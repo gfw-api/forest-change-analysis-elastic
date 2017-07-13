077a9006d50c1ceeec28c21b4b55c74ecaf1e75b
astrong19 <asastrong19@gmail.com>
2017-07-13 16:15:13 -0400

    new variables for response service

--- gladanalysis/services/response_service.py
+++ gladanalysis/services/response_service.py
@@ -5,7 +5,7 @@ class ResponseService(object):
     """Class for standardizing api responses"""
 
     @staticmethod
-    def standardize_response(name, data, datasetID, count=None, download_sql=None, area=None, geostore=None, agg=None):
+    def standardize_response(name, data, datasetID, count=None, download_sql=None, area=None, geostore=None, agg=None, agg_by=None, period=None):
         #Helper function to standardize API responses
         standard_format = {}
         if name == 'Glad':
@@ -14,11 +14,16 @@ class ResponseService(object):
         elif name == 'Terrai':
             standard_format["type"] = "terrai-alerts"
             standard_format["id"] = '{}'.format(os.getenv('TERRAI_DATASET_ID'))
+        if period:
+            standard_format['period'] = period
         standard_format["attributes"] = {}
         if agg:
+            standard_format['aggregate_values'] = True
             years = data.keys()
             for year in years:
                 standard_format["attributes"][year] = data[year]
+        if agg_by:
+            standard_format['aggregate_by'] = agg_by
         elif agg == None:
             standard_format["attributes"]["value"] = data["data"][0][count]
         if download_sql:
@@ -26,6 +31,7 @@ class ResponseService(object):
             standard_format["attributes"]["downloadUrls"]["csv"] = "/download/" + datasetID + download_sql + "&format=csv"
             standard_format["attributes"]["downloadUrls"]["json"] = "/download/" + datasetID + download_sql + "&format=json"
         if geostore:
+            standard_format["geostore"] = geostore
             standard_format["attributes"]["downloadUrls"]["csv"] += "&geostore=" + geostore
             standard_format["attributes"]["downloadUrls"]["json"] += "&geostore=" + geostore
         if area:
